08b1c3acc5fa542588bd6eede40cf668d4269f79
Lindsey11 <drewlindsey017@gmail.com>
2022-05-22 21:07:53 +0200

    first commit

---
+++ ODINVisionV2/ODINVisionV2/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ODINVisionV2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var url = URL.Text;
+            var station = Station.Text;
+
+            var answer = await DisplayAlert("Confirm", $"{url}/?stationid={station}", "Confirm", "Cancel");
+
+            if (answer)
+            {
+                await Navigation.PushAsync(new SequenceView(url, station));
+            }
+          
+        }
+    }
+}
+
---
+++ ODINVisionV2/ODINVisionV2/SequenceView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ODINVisionV2
+{	
+	public partial class SequenceView : ContentPage
+	{
+		public string FullUrl = "";
+		public SequenceView (string url, string stationID)
+		{
+			InitializeComponent ();
+			//FullUrl = 
+			pageviewer.Source = $"{url}/?stationid={stationID}";
+		}
+	}
+}
+
